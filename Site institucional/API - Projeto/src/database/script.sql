ceb765e3404d4e4ee1508c8c5e214fb1f1250ff6
EduardoPulcino <eduardo.pulcino@sptech.school>
2023-12-04 21:38:48 -0300

    Versão final do projeto

--- Site institucional/API - Projeto/src/database/script.sql
+++ Site institucional/API - Projeto/src/database/script.sql
@@ -30,58 +30,18 @@ create table contato (
     fkUsuario int,
     foreign key (fkUsuario) references usuario (idUsuario)
 );
- select idUsuario from usuario where email = 'teste@teste.com' LIMIT 1; 
-select * from usuario;
-select * from publicacao;
-select * from contato;
 
-select artista, count(artista) as quantidade from publicacao group by artista order by quantidade desc; 
-select instrumento, count(instrumento) as quantidade from publicacao group by instrumento order by quantidade desc;
-select genero, count(genero) as quantidade from publicacao group by genero order by quantidade desc;
-select usuario.username, p.instrumento, p.artista, p.genero, p.urlPDF, p.fkUsuario from publicacao as p join usuario on fkUsuario = idUsuario order by idPublicacao desc;
-DELETE FROM publicacao WHERE idPublicacao = 12;
-select artista, count(artista) as quantidade from publicacao group by artista order by quantidade desc limit 5;
-SELECT 
-    genero, 
-    COUNT(genero) AS quantidade,
-    SUM(quantidade) OVER () AS total_de_registros
-FROM (
-    SELECT genero, COUNT(genero) AS quantidade 
-    FROM publicacao 
-    GROUP BY genero
-) AS subquery
-GROUP BY genero
-ORDER BY quantidade desc;
-
-SELECT 
-        todos_generos.genero, 
-        COUNT(publicacao.genero) AS quantidade,
-        SUM(COUNT(publicacao.genero)) OVER () AS total
-    FROM (
-        SELECT DISTINCT genero 
-        FROM publicacao
-    ) AS todos_generos
-   JOIN publicacao ON todos_generos.genero = publicacao.genero
-    GROUP BY todos_generos.genero;
+insert into usuario values 
+	(null, 'Eduardo', 'Pulcino', 'eduardo.pulcino@sptech.school', 'Eduardo', 'Teste123@');
     
-    select artista, count(artista) as quantidade from publicacao group by artista order by quantidade desc limit 5;
-
-SELECT 
-    CASE 
-        WHEN RANK() OVER (ORDER BY quantidade DESC) <= 4 THEN genero
-        ELSE 'Outros'
-    END AS genero,
-    SUM(quantidade) AS quantidade
-FROM (
-    SELECT 
-        todos_generos.genero, 
-        COUNT(publicacao.genero) AS quantidade
-    FROM (
-        SELECT DISTINCT genero 
-        FROM publicacao
-    ) AS todos_generos
-    JOIN publicacao ON todos_generos.genero = publicacao.genero
-    GROUP BY todos_generos.genero
-) AS contagem_por_genero
-GROUP BY genero
-ORDER BY SUM(quantidade) DESC;
+insert into publicacao values 
+	(null, 'piano', 'Tim Maia', 'MPB', 'https://drive.google.com/file/d/14akZR1QZwKJMldtxyWqfol1puwgzLtLR/preview', 1),
+    (null, 'piano', 'Michael Jackson', 'POP', 'https://drive.google.com/file/d/14akZR1QZwKJMldtxyWqfol1puwgzLtLR/preview', 1),
+    (null, 'trombone', 'Bruno Mars', 'POP', 'https://drive.google.com/file/d/14akZR1QZwKJMldtxyWqfol1puwgzLtLR/preview', 1),
+    (null, 'piano', 'AC/DC', 'Rock', 'https://drive.google.com/file/d/14akZR1QZwKJMldtxyWqfol1puwgzLtLR/preview', 1),
+    (null, 'saxofone', 'Bruno Mars', 'POP', 'https://drive.google.com/file/d/14akZR1QZwKJMldtxyWqfol1puwgzLtLR/preview', 1),
+    (null, 'saxofone', 'Bruno Mars', 'POP', 'https://drive.google.com/file/d/14akZR1QZwKJMldtxyWqfol1puwgzLtLR/preview', 1),
+    (null, 'violino', 'Queen', 'Rock', 'https://drive.google.com/file/d/14akZR1QZwKJMldtxyWqfol1puwgzLtLR/preview', 1),
+    (null, 'violao', 'Aline Barros', 'Gospel', 'https://drive.google.com/file/d/14akZR1QZwKJMldtxyWqfol1puwgzLtLR/preview', 1),
+    (null, 'violao', 'Adoniran Barbosa', 'Samba', 'https://drive.google.com/file/d/14akZR1QZwKJMldtxyWqfol1puwgzLtLR/preview', 1),
+    (null, 'saxofone', 'Adoniran Barbosa', 'Samba', 'https://drive.google.com/file/d/14akZR1QZwKJMldtxyWqfol1puwgzLtLR/preview', 1);
